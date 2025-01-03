8150417028a779c6f05b7f54ee3d7018783a699b
traison <traison.diedrich@mdlive.com>
2025-01-03 17:24:32 -0600

    barbeque and lualine

---
+++ .config/nvim/lua/core/plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    options = {
+      theme = 'gruvbox',
+    },
+    config = function()
+      require('lualine').setup()
+    end,
+  },
+}
